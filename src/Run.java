c2723da73923c2bb7c0fb61d6d2bf7d42381f70f
Giuseppe Abbate <giuseppeabbatte94@gmail.com>
2015-10-19 13:59:55 +0200

    Så jyderne også kan være med :D

--- src/Run.java
+++ src/Run.java
@@ -5,6 +5,7 @@ public class Run {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World!");
 		System.out.println("Rullekebab");
+		System.out.println("Kikkertsandwich");;
 
 	}
 
